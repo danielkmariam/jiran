05038ee01bae5ed986d6a7777b2fd3aa09d6ccf9
danielkmariam <danielkmariam@gmail.com>
2016-01-03 20:04:04 +0000

    Dry table renderer

--- src/table_renderer.js
+++ src/table_renderer.js
@@ -9,16 +9,15 @@ class TableRenderer {
 	}
 
 	renderVertical (body) {
-		let table = new Table(this.header)
-		for (let item of body) {
-	    table.push(item)
-	  }
-    console.log(table.toString())
+		this.renderTable(body)
 	}
 
 	render (head, body) {
 		this.header.head = head
-		
+		this.renderTable(body)
+	}
+
+	renderTable (body) {
 		let table = new Table(this.header)
 		for (let item of body) {
 	    table.push(item)
